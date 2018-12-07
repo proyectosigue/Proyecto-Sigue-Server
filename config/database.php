d6d365188c96d4112821200982a538fa30a8c9a4
rodrigosanchezg8 <rodrigosanchezg8@gmail.com>
2018-12-06 20:57:07 -0600

    Heroku config

--- config/database.php
+++ config/database.php
@@ -120,7 +120,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+            'user' => env('REDIS_USER', 6379)
         ],
 
     ],
